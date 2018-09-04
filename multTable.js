0014be6af154a9a9d001cb201d4e91c397afe04c
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2018-09-04 17:17:32 -0500

    Project Complete

--- multTable.js
+++ multTable.js
@@ -1,25 +1,38 @@
   /*
-      Developer:   Frankie Barrios
-      Date:        09/04/18
+        Developer:   Frankie Barrios
+        Date:        09/04/18
 
-      Purpose: Write a program that shows 
-      all numbers between 1 and 100 with the 
-      following exceptions:
-      It shows "Fizz"
-      instead if the number is divisible by 3.
-      It shows "Buzz" instead if the number is 
-      divisible by 5 and not by 3.
-      When it 's done, improve it so that the 
-      program shows "FizzBuzz" instead for numbers 
-      divisible both by 3 and by 5.
-      
-      */
+        Purpose: Write a program that shows 
+        all numbers between 1 and 100 with the 
+        following exceptions:
+        It shows "Fizz"
+        instead if the number is divisible by 3.
+        It shows "Buzz" instead if the number is 
+        divisible by 5 and not by 3.
+        When it 's done, improve it so that the 
+        program shows "FizzBuzz" instead for numbers 
+        divisible both by 3 and by 5.
+        
+        */
 
-  for (var i = 1; i <= 100; i++) 
-  {
+  /* for (var i = 1; i <= 100; i++) {
       if (i % 3 == 0)
           console.log("Fizz");
       else if (i % 5 == 0)
+          console.log("Buzz");
+      else
+          console.log(i);
+  }
+ */
+
+  for (var i = 1; i <= 100; i++) 
+  {
+
+      if (i % 3 == 0 && i % 5 == 0)
+          console.log("FizzBuzz");
+      else if (i % 3 == 0)
+          console.log("Fizz");
+      else if (i % 5 == 0 && i % 3 != 0)
           console.log("Buzz");
       else
           console.log(i);
