d677353d2bad58d3738320b7bb16426c28cc5c0a
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2018-09-04 16:52:01 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let a = 2;
+
+a -= 1;
+
+a++;
+
+let b = 8;
+
+b += 2;
+
+const c = a + b * b;
+//console.log(c);
+
+const d = a * b + b;
+//console.log(d);
+
+const e = a * (b + b);
+//console.log(e);
+
+const f = a * b / a;
+//console.log(f);
+
+const g = b / a * a;
+//console.log(g);
+
+console.log(a, b, c, d, e, f, g);
