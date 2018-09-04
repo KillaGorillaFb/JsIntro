bac211f330717bc4562f4a472829c1d5e0c3cef9
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2018-09-04 17:02:41 -0500

    Temperature Conversion Complete

---
+++ degrees.js
@@ -0,0 +1,28 @@
+  /*
+  Developer:   Frankie Barrios
+  Date:        09/04/18
+  Purpose:     Write a program that assigns
+  to a variable a temperature in Celsius degrees, 
+  then displays it in Fahrenheit degrees.
+  */
+
+ var celcius = 10;
+ var celToFar = celcius * 9 / 5 + 32;
+ console.log(celToFar);
+
+ /*
+   Developer:   Frankie Barrios
+   Date:        09/04/18
+
+   Purpose: Write a program that shows 
+   all numbers between 1 and 100 with the 
+   following exceptions:
+   It shows "Fizz"
+   instead if the number is divisible by 3.
+   It shows "Buzz" instead if the number is 
+   divisible by 5 and not by 3.
+   When it 's done, improve it so that the 
+   program shows "FizzBuzz" instead for numbers 
+   divisible both by 3 and by 5.
+   
+   */
